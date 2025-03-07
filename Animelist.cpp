6e22c0eb560618244a504cf36465e5bbcee47f71
Shreedarsh-Nair <shreedarshnair@gmail.com>
2025-03-07 15:17:44 +0530

    Update Animelist.cpp
    Added deletion and display functionality

--- Animelist.cpp
+++ Animelist.cpp
@@ -43,37 +43,41 @@ int main(){
 		std::cin >> opt;
 		switch(opt){
 			case 0:
-				{
-					std::cout << "Enter the name of anime: ";
-					std::cin >> std::ws;
-					std::getline(std::cin, name);
-					a_list.insert(name);
-	
-					std::ofstream ofile("Animelist.txt", std::ios::trunc);
-					if(!ofile){
-						std::time_t timestamp;
-						time(&timestamp);
-						
-						std::ofstream errfile("Error.txt", std::ios::app);
-						if(!errfile){
-							std::cout << "Couldn't open error file.";
-							return -1;
-						}
-						errfile << ctime(&timestamp) << " Error opening file to read.";
-						errfile.close();
-				
-						return -1;
-					}
+				std::cout << "Enter the name of anime: ";
+				std::cin >> std::ws;
+				std::getline(std::cin, name);
+				a_list.insert(name);
 
-					for(std::string a_name : a_list){
-						ofile << a_name << std::endl;
-					}
+				if(write_file(a_list)){
+					std::cout << "Error in writing to file." << std::endl;
+				}	
 
-					ofile.close();
+				break;
+
+			case 1:
+				std::cout << "Enter name of anime: ";
+				std::cin >> std::ws;
+				std::getline(std::cin, name);
+				a_list.erase(name);
+
+				if(write_file(a_list)){
+					std::cout << "Error in writing to file." << std::endl;
 				}
 
 				break;
 
+			case 2:
+			{
+				int i = 1;
+				
+				for(std::string anime : a_list){
+					std::cout << i << ". "  << anime << std::endl;
+					i++;
+				}
+			}
+			
+				break;
+			
 			case 3:
 				return 0;
 		
@@ -102,7 +106,7 @@ int write_file(std::set<std::string> ani_list){
 		return -1;
 	}		
 	for(std::string anime : ani_list){
-		ofile << anime;
+		ofile << anime << std::endl;
 	}
 	
 	ofile.close();
